923960a6a196206601840beea36c7a03032911b2
Mikhail Polovnikov <mishapolovnikov@gmail.com>
2024-12-17 15:06:10 +0300

    change main.oy

--- main.py
+++ main.py
@@ -103,49 +103,49 @@ def average_lecturer_grade(lecturers, course):
 
 # Полевые испытания
 # Студенты
-# student1 = Student('Ruoy', 'Eman', 'male')
-# student1.courses_in_progress += ['Python', 'Git']
-# student1.finished_courses += ['Введение в программирование']
-#
-# student2 = Student('Jane', 'Doe', 'female')
-# student2.courses_in_progress += ['Python']
-# student2.finished_courses += ['Git']
-#
-# # Лекторы
-# lecturer1 = Lecturer('John', 'Smith')
-# lecturer1.courses_attached += ['Python']
-#
-# lecturer2 = Lecturer('Alice', 'Brown')
-# lecturer2.courses_attached += ['Git']
-#
-# # Проверяющие
-# reviewer1 = Reviewer('Some', 'Buddy')
-# reviewer1.courses_attached += ['Python']
-#
-# reviewer2 = Reviewer('Ann', 'Smith')
-# reviewer2.courses_attached += ['Git']
-#
-# # Выставляем оценки
-# reviewer1.rate_hw(student1, 'Python', 9)
-# reviewer1.rate_hw(student1, 'Python', 10)
-# reviewer1.rate_hw(student2, 'Python', 8)
-#
-# reviewer2.rate_hw(student1, 'Git', 7)
-#
-# student1.rate_lecturer(lecturer1, 'Python', 10)
-# student1.rate_lecturer(lecturer1, 'Python', 9)
-# student2.rate_lecturer(lecturer1, 'Python', 8)
-#
-# student1.rate_lecturer(lecturer2, 'Git', 10)
-#
-# # Вывод результатов
-# print(reviewer1)
-# print(lecturer1)
-# print(student1)
-#
-# print(f'Средняя оценка студентов по Python: {average_student_grade([student1, student2], "Python"):.1f}')
-# print(f'Средняя оценка лекторов по Python: {average_lecturer_grade([lecturer1, lecturer2], "Python"):.1f}')
-#
-# # Сравнение студентов и лекторов
-# print(student1 > student2)  # True/False
-# print(lecturer1 < lecturer2)  # True/False
+student1 = Student('Ruoy', 'Eman', 'male')
+student1.courses_in_progress += ['Python', 'Git']
+student1.finished_courses += ['Введение в программирование']
+
+student2 = Student('Jane', 'Doe', 'female')
+student2.courses_in_progress += ['Python']
+student2.finished_courses += ['Git']
+
+# Лекторы
+lecturer1 = Lecturer('John', 'Smith')
+lecturer1.courses_attached += ['Python']
+
+lecturer2 = Lecturer('Alice', 'Brown')
+lecturer2.courses_attached += ['Git']
+
+# Проверяющие
+reviewer1 = Reviewer('Some', 'Buddy')
+reviewer1.courses_attached += ['Python']
+
+reviewer2 = Reviewer('Ann', 'Smith')
+reviewer2.courses_attached += ['Git']
+
+# Выставляем оценки
+reviewer1.rate_hw(student1, 'Python', 9)
+reviewer1.rate_hw(student1, 'Python', 10)
+reviewer1.rate_hw(student2, 'Python', 8)
+
+reviewer2.rate_hw(student1, 'Git', 7)
+
+student1.rate_lecturer(lecturer1, 'Python', 10)
+student1.rate_lecturer(lecturer1, 'Python', 9)
+student2.rate_lecturer(lecturer1, 'Python', 8)
+
+student1.rate_lecturer(lecturer2, 'Git', 10)
+
+# Вывод результатов
+print(reviewer1)
+print(lecturer1)
+print(student1)
+
+print(f'Средняя оценка студентов по Python: {average_student_grade([student1, student2], "Python"):.1f}')
+print(f'Средняя оценка лекторов по Python: {average_lecturer_grade([lecturer1, lecturer2], "Python"):.1f}')
+
+# Сравнение студентов и лекторов
+print(student1 > student2)  # True/False
+print(lecturer1 < lecturer2)  # True/False
